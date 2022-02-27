88db292676fb5b43ededf17afea737495498fe05
Rasneet Kaur <97087523+rasneetkaur10@users.noreply.github.com>
2022-02-27 18:08:14 +0530

    Add files via upload

---
+++ assignment 4 result.py
@@ -0,0 +1,86 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Dell/OneDrive/Desktop/pec classes/assignments/intro c/assignment 4.py
+Question 1 
+
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+********************************************************************************
+
+
+Question 2 
+
+Enter the size of pascals triangle 8
+USING LOOPS
+         1 
+        1 1 
+       1 2 1 
+      1 3 3 1 
+     1 4 6 4 1 
+    1 5 10 10 5 1 
+   1 6 15 20 15 6 1 
+  1 7 21 35 35 21 7 1 
+
+
+
+USING RECURSSIONS
+              1   
+            1   1   
+          1   2   1   
+        1   3   3   1   
+      1   4   6   4   1   
+    1   5   10   10   5   1   
+  1   6   15   20   15   6   1   
+1   7   21   35   35   21   7   1   
+
+
+********************************************************************************
+
+
+Question 3 
+
+Enter 2 numbers: 2 4
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> The quotient is zero
+<c> Filtered out numbers that are greater than 4 are : [6, 7, 5, 7, 6, 8]
+<d> Set: {8, 5, 6, 7}
+<e> Immutable set: frozenset({8, 5, 6, 7})
+<f> Hash value of the max value from the above set: 8
+
+
+********************************************************************************
+
+
+Question 4 
+
+Object created
+The name of the student it Rasneet Kaur and SID is 21104010.
+
+
+********************************************************************************
+
+
+Question 5 
+
+<a> The updated salary of Mehak is 70000 
+<b> Record of Viren deleted
+
+********************************************************************************
+
+
+Question 6 
+
+Enter the word: heart
+Enter a new meaningful word using the exact same letters to test your friendship: earth
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
